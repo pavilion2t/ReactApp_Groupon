962132776ae48eba2506075dca06d73930870c31
Judy <850832627@qq.com>
2017-09-25 21:17:47 +0800

    Update index.jsx

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
 import './style.less'
+/*
+头部左侧显示城市（后面还有链接功能），中间搜索，右侧图标点击跳转到个人主页
+Head 本身没有涉及到和后端数据的交互
+涉及不到后端数据交互，因此这个 Head 是木偶组件
 
+*/
 class HomeHeader extends React.Component {
     constructor(props, context) {
         super(props, context);
